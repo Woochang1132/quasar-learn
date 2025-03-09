a2859a0c044817aff89807ed421c0309e50a520f
Woochang1132 <show4031132@gmail.com>
2025-03-09 17:26:31 +0900

    [퀘이사] => breakpoint 적용하기

--- quasar.config.js
+++ quasar.config.js
@@ -79,7 +79,12 @@ export default defineConfig((/* ctx */) => {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
     framework: {
-      config: {},
+      cssAddon: true,
+      config: {
+        screen: {
+          bodyClasses: true, // <<< add this
+        },
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
