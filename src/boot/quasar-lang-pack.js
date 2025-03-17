4744e647fce73eebe0caac955a4a8813928307ed
Woochang1132 <show4031132@gmail.com>
2025-03-17 15:55:46 +0900

    [퀘이사] > 언어팩 default 값 설정, 수정완료

--- src/boot/quasar-lang-pack.js
+++ src/boot/quasar-lang-pack.js
@@ -2,7 +2,7 @@ import { boot } from 'quasar/wrappers';
 import { LocalStorage, Quasar } from 'quasar';
 
 export default boot(async () => {
-  const val = LocalStorage.getItem('lang');
+  const val = LocalStorage.getItem('lang') || Quasar.lang.isoName;
   import('../../node_modules/quasar/lang/' + val).then(lang => {
     Quasar.lang.set(lang.default);
   });
