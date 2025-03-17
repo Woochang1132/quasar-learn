5bc49a307359ba092059908eb4eafa68c5ea53d5
Woochang1132 <show4031132@gmail.com>
2025-03-17 16:11:21 +0900

    [versel] > depoly log 확인 후 소스 수정완료

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -1,6 +1,6 @@
 import { boot } from 'quasar/wrappers';
 import { createI18n } from 'vue-i18n';
-import Quasar, { LocalStorage } from 'quasar';
+import { LocalStorage, Quasar } from 'quasar';
 import messages from 'src/i18n';
 
 export default boot(async ({ app }) => {
